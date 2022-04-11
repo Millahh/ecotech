ec02c1a54f55553a47579c60a284e1e1fe8cef10
DESKTOP-SSMAHEL\ASUS <rabiatulauliah06@gmail.com>
2022-04-11 08:58:32 +0700

    landing page

---
+++ resources/views/partials/navbar-logged.blade.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-dark bg-dark navbar-expand-sm">
+  <div class="collapse navbar-collapse ml-auto justify-content-end me-4" id="navbar-list-4">
+    <ul class="navbar-nav">
+        <li class="nav-item dropdown">
+        <a class="nav-link d-flex dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <img src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/fox.jpg" width="40" height="40" class="rounded-circle me-3">
+          <h6>rara</h6>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Dashboard</a>
+          <a class="dropdown-item" href="#">Edit Profile</a>
+          <a class="dropdown-item" href="#">Log Out</a>
+        </div>
+      </li>   
+    </ul>
+  </div>
+</nav>
